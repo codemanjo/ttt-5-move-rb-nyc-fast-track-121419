ebc0db3bcc739c9b7a6b8aa043c37af97a543a44
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-12 17:14:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,7 +9,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index          # code your input_to_index and move method here!
-  
+def input_to_index(move)         # code your input_to_index and move method here!
+  move.to_i - 1
 
 end
